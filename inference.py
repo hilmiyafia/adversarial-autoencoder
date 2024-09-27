7088754ffbdb42e85ce38ded20db6529627b3997
Hilmi Yafi A <56539775+hilmiyafia@users.noreply.github.com>
2024-09-27 13:45:20 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -16,7 +16,9 @@ if __name__ == "__main__":
 
     with torch.no_grad():
         random = torch.randn(ROWS*COLS, 256)
+        random = random.as_type(model.autoencoder.decoder[-1].layers[0].weight)
         random = model.autoencoder.decoder(random)
         random = torch.concat([torch.concat([random[i+j*ROWS] for i in range(ROWS)], 1) for j in range(COLS)], 2)
         random = (random * 255).clamp(min=0, max=255).to(torch.uint8)
         torchvision.io.write_jpeg(random, "result.jpg")
+
